66f23fb80bb55cd2f21f75e6f06e369f088140cc
leandre <tuyambazeleandre@gmail.com>
2024-01-03 19:33:04 +0200

    (day-2): destructure goalItem component

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import {
   ScrollView,
   FlatList,
 } from 'react-native';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
   const [goalText, setGoalText] = useState('');
@@ -39,11 +40,7 @@ export default function App() {
           alwaysBounceVertical={false}
           data={courseGoals}
           renderItem={(itemData) => {
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-              </View>
-            );
+            return <GoalItem text={itemData.item.text} />;
           }}
           keyExtractor={(item) => item.id}
         ></FlatList>
@@ -77,14 +74,5 @@ const styles = StyleSheet.create({
 
   goalsContainer: {
     flex: 5,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e0acc',
-  },
-  goalText: {
-    color: 'white',
   },
 });
