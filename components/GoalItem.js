99d8aaf0601a283805682bdbc1e728cae342b27e
leandre <tuyambazeleandre@gmail.com>
2024-01-03 20:31:36 +0200

    (day-2): change color when goal card is pressed

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,7 +3,11 @@ import { View, StyleSheet, Text, Pressable } from 'react-native';
 const GoalItem = ({ goal, onDeleteItem }) => {
   return (
     <View style={styles.goalItem}>
-      <Pressable onPress={onDeleteItem.bind(this, goal.id)}>
+      <Pressable
+        android_ripple={{ color: '#210644' }}
+        onPress={onDeleteItem.bind(this, goal.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{goal.text}</Text>
       </Pressable>
     </View>
@@ -17,6 +21,9 @@ const styles = StyleSheet.create({
     margin: 8,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
+  },
+  pressedItem: {
+    opacity: 0.5,
   },
   goalText: {
     color: 'white',
